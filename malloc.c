9b41f4391bc4abd59e5e375de5d87aa7efe60154
Max Gautier <mgautier@student.42.fr>
2017-05-24 18:43:16 +0200

    Thingss

--- malloc.c
+++ malloc.c
@@ -6,21 +6,24 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/24 13:48:51 by mgautier          #+#    #+#             */
-/*   Updated: 2017/05/24 15:40:40 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/05/24 16:39:16 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stddef.h>
+#include <stdlib.h>
 #include <dlfcn.h>
 #ifndef CALL_TO_MALLOC_BEFORE_FAILURE
-# define CALL_TO_MALLOC_BEFORE_FAILURE 100
+# define CALL_TO_MALLOC_BEFORE_FAILURE atoi(getenv("MALLOC_TEST"))
 #endif
 
 void *malloc(size_t size)
 {
 	static int	count = 0;
 	void * (*real_malloc)(size_t);
+	void	*address;
+	char	desd;
 
 	count++;
 	ft_putstr("Malloc called : ");
@@ -28,7 +31,10 @@ void *malloc(size_t size)
 	ft_putchar('\n');
 	real_malloc = dlsym(RTLD_NEXT, "malloc");
 	if (count < CALL_TO_MALLOC_BEFORE_FAILURE)
-	return real_malloc(size);
+	address = real_malloc(size);
 	else
-		return (NULL);
+		address = NULL;
+	if (getenv("DEREF_MAL") != NULL)
+		desd = *((char*)address);
+	return (address);
 }
